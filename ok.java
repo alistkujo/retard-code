68b7ea5474c4e7e18066443901b021801630b009
alistkujo <74325635+alistkujo@users.noreply.github.com>
2020-11-13 18:23:22 +0000

    ok
    i like dolphins fuck you

---
+++ ok.java
@@ -0,0 +1,15 @@
+
+public class ok {
+
+	public static void main(String[] args) {
+		
+		animal b = new animal();
+		String Dolphin = b.iAmDolphin();
+		System.out.println(Dolphin);
+		int a = 5;
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Dolphins");
+		}
+	}
+		
+}
