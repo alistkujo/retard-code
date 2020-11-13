38001bc7e2e1424ed80d2b159dd75484087d3c10
alistkujo <74325635+alistkujo@users.noreply.github.com>
2020-11-13 22:08:33 +0000

    johnny the cripple approves

---
+++ Pizza.java
@@ -0,0 +1,24 @@
+
+public class Pizza {
+
+	public static void main(String[] args) {
+		int a1 = 5;
+		for(int i = 0; i < 2; i++) {
+			System.out.println("Pizza Mozzarella\n");
+		}
+		int a = 5;
+		for(int i = 0; i < 3; i++) {
+			System.out.println("Rella Rella\n");
+		}
+		System.out.println("Pizza Mozzarella\n");
+		int b = 5;
+		for(int i = 0; i < 2; i++) {
+			System.out.println("Pizza Gorgonzola\n");
+		}
+		int c = 5;
+		for(int i = 0; i < 3; i++) {
+			System.out.println("Zorla Zorla\n");
+		}
+		System.out.println("Pizza Mozzarella");
+	}
+}
